ad14dd30d1ef6d4fabac16f36e308b4a8dcb4a2a
Akanksha Parmar <iakankshaparmar>
2023-12-22 12:55:09 +0530

    Initial Commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Akanksha Parmar on 22/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
